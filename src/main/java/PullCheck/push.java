20bcee9570fab22594dcb61f8f10841cd58854ae
suraj <shindesuraj643@gmail.com>
2022-07-30 19:38:29 +0530

    new class push added

---
+++ src/main/java/PullCheck/push.java
@@ -0,0 +1,10 @@
+package PullCheck;
+
+public class push {
+public static void main(String[] args) {
+	//  src/main/java/Reqres/
+//restassure.api="http://locslhost:8088"
+//System.out.println(changes in locsl host);
+
+}
+}
